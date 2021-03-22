a6716c814c1faa98fd323866691f1322864230ed
Santiago Bedoya <santiago.b9826@gmail.com>
2021-03-22 00:11:32 -0500

    Added middlewares to not found resources (Adding a custom error class) and return formatted errors. Added http-status dependency. Added codes in config.

---
+++ App/src/errors/not-found.error.js
@@ -0,0 +1,21 @@
+const { BAD_REQUEST } = require('http-status');
+const { NOT_FOUND_CODE } = require('../../config/codes.config');
+
+class NotFoundError extends Error {
+  /**
+   * @class Extends from Error class
+   * @param {Number} status Error http status to response.
+   * @param {String} message Descriptive message about the error generated.
+   */
+  constructor() {
+    const message = 'The requested resource was not found';
+    super(message);
+
+    this.code = NOT_FOUND_CODE;
+    this.status = BAD_REQUEST;
+  }
+}
+
+module.exports = {
+  NotFoundError
+};
